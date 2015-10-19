2ad3f122eaed263327e06246b2c351ab0b8eb474
Vikrant Negi <vikrantnegi007@gmail.com>
2015-10-19 17:16:09 +0530

    Update Bonfire - 03 - Factorialize a Number.js
    I think it would be better if we don't use the in-built function in the JS and define by themselves to better have grasp of how things work in a better way,

--- 06 - basic-algorithm-scripting/Bonfire - 03 - Factorialize a Number.js
+++ 06 - basic-algorithm-scripting/Bonfire - 03 - Factorialize a Number.js
@@ -1,14 +1,8 @@
 function factorialize(num) {
-  switch(num) {
-    case 0:
-      return 1;
-      break;
-    case 1: 
-      return num;
-      break;
-    default:
-      return num * factorialize(num - 1);
+  for(i=num-1; i>0; i--){
+    num=num*i
   }
+  return num;
 }
 
-factorialize(5, '');
+factorialize(5);
