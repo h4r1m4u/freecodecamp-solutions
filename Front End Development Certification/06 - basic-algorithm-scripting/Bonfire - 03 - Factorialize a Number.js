359387e31f6724df6dc6ed83a45bbd613a27e97a
Larry D'Almeida <hello@larrydalmeida.com>
2016-11-18 09:46:53 +0530

    A slightly more optimal solution
    Removed redundant conditions by replacing switch

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 03 - Factorialize a Number.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 03 - Factorialize a Number.js
@@ -1,14 +1,6 @@
 function factorialize(num) {
-  switch(num) {
-    case 0:
-      return 1;
-      break;
-    case 1: 
-      return num;
-      break;
-    default:
-      return num * factorialize(num - 1);
-  }
+  if (num === 0) { return 1; }
+  return num * factorialize(num-1);
 }
 
 factorialize(5, '');
