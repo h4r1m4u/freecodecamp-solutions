8fe56de739f7c30893168f95942a72793b9fa4d3
Larry D'Almeida <hello@larrydalmeida.com>
2016-11-18 13:43:08 +0530

    Optimized solution

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 11 - Chunky Monkey.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 11 - Chunky Monkey.js
@@ -1,22 +1,12 @@
-function chunk(arr, size) {
-  // Break it up.
-  var result = [];
-  // While arr has more elements than should be in a single chunk
-  while (size <= arr.length) {
-    var chunk = [];
-    for (var i = 0; i < size; i++) {
-      // Remove the required number of elements from arr and store them in chunk array
-      chunk.push(arr.shift());
-    }
-    // Push the chunk into result
-    result.push(chunk);
+function chunkArrayInGroups(arr, size) {
+  // Break it up
+  var newArr = [], i = 0;
+  
+  while(i < arr.length) {
+    newArr.push(arr.slice(i, i += size));
   }
-  // If, after the removal of a number of elements, arr still has any elements left, 
-  // push them into result. This will happen if arr % size != 0, e.g. arr = [1, 2, 3] and size = 2.
-  if (arr.length > 0) {
-    result.push(arr);
-  }
-  return result;
+  
+  return newArr;
 }
 
 chunk(["a", "b", "c", "d"], 2, "");
