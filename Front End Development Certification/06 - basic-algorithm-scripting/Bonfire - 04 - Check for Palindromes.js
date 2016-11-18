eccd590eb2a68949970385918f534c4e0ab83a61
h4r1m4u <h4r1m4u@users.noreply.github.com>
2016-11-18 12:58:39 +0800

    Update Bonfire - 04 - Check for Palindromes.js

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 04 - Check for Palindromes.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 04 - Check for Palindromes.js
@@ -1,9 +1,8 @@
 function palindrome(str) {
-  // Good luck!
+  // Remove punctuation and make the string lowercase
   str = str.replace(/[\W_]+/g, "").toLowerCase();
+  // Reverse the string and check for palindrome
   return str === str.split("").reverse().join("");
 }
-
-
 
 palindrome("eye");
