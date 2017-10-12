2b137e67082116f1dda28eae63a9239c1b9509e9
Pritesh G <pritesh@wow.com>
2017-10-12 21:47:50 +0530

    Updated the code for more test cases
    Previous code no longer works.

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
@@ -1,10 +1,11 @@
-function truncate(str, num) {
+function truncateString(str, num) {
   // Clear out that junk in your trunk
-  if (str.length > num) {
-    return str.slice(0, num - 3).concat('...');
+  if (str.length > num && num > 3) {
+    return str.slice(0, (num - 3)) + '...';
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num) + '...';
   } else {
     return str;
   }
 }
-
 truncate("A-tisket a-tasket A green and yellow basket", 11, "");
