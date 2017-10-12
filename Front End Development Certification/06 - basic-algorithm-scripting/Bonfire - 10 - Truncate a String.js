2602e084bc70c6691b45622a3cec445c6ef97dab
Richard Cabelka <richard.cabelka@gmail.com>
2017-10-13 02:23:30 +0800

    Remove unnecessary if/else branches. Use assignment operators to join strings instead of the concat() method for performance reasons.

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
@@ -1,11 +1,12 @@
 function truncateString(str, num) {
   // Clear out that junk in your trunk
-  if (str.length > num && num > 3) {
-    return str.slice(0, (num - 3)) + '...';
-  } else if (str.length > num && num <= 3) {
+  if (str.length > num && num <= 3) {
     return str.slice(0, num) + '...';
-  } else {
-    return str;
   }
+  if (str.length > num) {
+    return str.slice(0, num - 3) + '...';
+  } 
+  return str;
 }
+
 truncate("A-tisket a-tasket A green and yellow basket", 11, "");
