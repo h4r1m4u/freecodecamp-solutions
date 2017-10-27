43b011ca2cba0cacbc730deaee5a836960f8cbbb
h4r1m4u <richard.cabelka@gmail.com>
2017-10-27 23:55:09 +0800

    Stylistic improvements.

--- Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 03 - Roman Numeral Converter.js
+++ Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 03 - Roman Numeral Converter.js
@@ -1,12 +1,13 @@
 function convertToRoman(num) {
-var map = { M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 };
-var roman ='';
-for(var i in map){
-  while(num >= map[i]){
-    roman += i;
-    num -= map[i];
+  var map = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
+  var roman = '';
+  for ( var i in map ) {
+    while ( num >= map[i] ) {
+      roman += i;
+      num -= map[i];
+    }
   }
+  return roman;
 }
-return roman;
-}
+
 convertToRoman(500);
