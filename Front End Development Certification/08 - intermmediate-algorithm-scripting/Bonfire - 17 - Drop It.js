3975deb70d6f859ebaa5f84f31b4275fbca78d42
h4r1m4u <h4r1m4u@users.noreply.github.com>
2017-11-01 12:55:07 +0800

    Intermmediate Algorithm Scripting - 17 Drop It - Add new solution that satisfies changes in the challenge assignment.

--- Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 17 - Drop It.js
+++ Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 17 - Drop It.js
@@ -1,12 +1,16 @@
-function drop(arr, func) {
+function dropElements(arr, func) {
   // Drop them elements.
-  var result = [];
-  for (var i = 0; i <= arr.length - 1; i++) {
-    if (func(arr[i]) == true) {
-      result.push(arr[i]);
-    }
+
+  // Find the index of the first element for which func returns true.
+  var firstNotDropped = arr.findIndex(func);
+
+  // If such element is in the array, return a slice
+  // of the array from that element onwards. 
+  // Otherwise return an empty array.
+  if (firstNotDropped > -1) {
+    return arr.slice(firstNotDropped);
   }
-  return result;
+  return [];  
 }
 
-drop([1, 2, 3], function(n) {return n < 3; });
+dropElements([1, 2, 3], function(n) {return n < 3; });
