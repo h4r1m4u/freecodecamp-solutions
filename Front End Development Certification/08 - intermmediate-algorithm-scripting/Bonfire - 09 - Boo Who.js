286ee5011a3471be10e6e99b5eebc7d06eac78a9
h4r1m4u <h4r1m4u@users.noreply.github.com>
2017-11-01 12:27:07 +0800

    Intermmediate Algorithm Scripting - 09 Boo Who - Remove unnecessary 'else' branch.

--- Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 09 - Boo Who.js
+++ Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 09 - Boo Who.js
@@ -2,8 +2,7 @@ function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
   if (typeof bool === 'boolean') {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 booWho(null);
