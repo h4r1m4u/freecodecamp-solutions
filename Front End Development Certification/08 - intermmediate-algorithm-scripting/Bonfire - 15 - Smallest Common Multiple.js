ce487649f4b7e056c2be065b676cad9d6fee4e44
tiagosalema <tiagof.salema@gmail.com>
2019-04-20 18:09:19 +0100

    Added a solution suggestion for the ex.15

--- Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 15 - Smallest Common Multiple.js
+++ Front End Development Certification/08 - intermmediate-algorithm-scripting/Bonfire - 15 - Smallest Common Multiple.js
@@ -1,40 +1,18 @@
-function smallestCommons(arr) {
-  // sort the array descendingly
-  arr = arr.sort(function(a, b) {
-    return a - b;
-  });
-  
-  var numberOfFoundCommons = 0; // tracks how many common multiples we've found
-  var commons = arr[0]; // the value of found common multiple; by default the highest element in our array
-  
-  // generate array that has all the numbers in the range we need to test
-  var range = [];
-  for (var i = arr[0]; i <= arr[1]; i++) {
-      range.push(i);    
-  }
-  
-  // do this until numberOfFoundCommons equals the number of elements in range 
-  // (i.e. the common multiple is a common multiple for all the numbers in the range)
-  while (numberOfFoundCommons != range.length) {
-    // increase the common multiple by the largest number in our range
-    commons += range[0];
-    numberOfFoundCommons = 0;
-	var j = 0;
-    // test the common multiple for all the numbers in the range
-    while (j != range.length) {
-        if (commons % range[j] == 0) {
-            // the common multiple is a multiple of this number
-        	numberOfFoundCommons += 1;
-        } else {
-            // the common multiple is not a multiple of this number and we can break out of the loop
-            // as there is no point in testing the rest of the numbers
-            break;
-        }
-        j++;
+const smallestCommons = arr => {
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  // Initially the solution is assigned to the highest value of the array
+  let sol = max;
+
+  for (let i = max - 1; i >= min; i--) {
+    // Each time the solution checks (i.e. sol%i===0) it won't be necessary
+    // to increment 'max' to our solution and restart the loop
+    if (sol % i) {
+      sol += max;
+      i = max;
     }
   }
-  
-  return commons;
-}
+  return sol;
+};
 
-smallestCommons([1,5]);
+smallestCommons([1, 5]);
