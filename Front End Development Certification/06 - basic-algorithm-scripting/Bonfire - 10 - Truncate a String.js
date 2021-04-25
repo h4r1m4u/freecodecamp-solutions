2d1ff67fe5cf31eae1edbb9fa65cdf2efadf83da
Javier Valeriano <77125276+javiervaleriano@users.noreply.github.com>
2021-04-25 12:35:37 -0400

    Update Bonfire - 10 - Truncate a String.js
    The requested changes have been made.

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
@@ -1,12 +1,8 @@
 function truncateString(str, num) {
-  /* If the number of characters in the string
-   * is greater than the num parameter,
-   * then it will truncate the string */
   if(str.length > num) {
     return str.slice(0, num) + '...';
-  } else {
-    return str;
   }
+    return str;
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
