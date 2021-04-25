8a2a1514b91c7287f8d07859c1a8f9e07bb0e359
h4r1m4u <h4r1m4u@users.noreply.github.com>
2021-04-26 00:42:50 +0800

    Spacing and indentation adjustments after a PR for cleaner code.

--- Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
+++ Front End Development Certification/06 - basic-algorithm-scripting/Bonfire - 10 - Truncate a String.js
@@ -1,8 +1,9 @@
 function truncateString(str, num) {
-  if(str.length > num) {
+  if (str.length > num) {
     return str.slice(0, num) + '...';
   }
-    return str;
+  
+  return str;
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
